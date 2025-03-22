f89ee9298b395395214d88b9263dcecd45d25886
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-03-22 17:04:30 +0100

    add server root URL, /api URL

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 import express from "express";
 
+// Create an express server
 const app = express();
-const server = app.listen(3000, () => {
-  console.log("The server is listening... on port 3000...🐒");
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// Create a route (GET) to the root URL
+app.get("/", (req, res) => {
+  res.send("Hello from the server side...🐒");
+});
+
+// Create a route (GET) to the /api URL
+app.get("/api", (req, res) => {
+  res.send("Hello from the API side...🐒");
 });
